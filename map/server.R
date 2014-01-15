bd2c1f68a15c2ef58dc0a4806ab12cb827c8eb5f
chaid <chaid@kippchicago.org>
2014-01-15 22:46:31 +0000

    Adding files to ShinyApps/map

---
+++ map/server.R
@@ -0,0 +1,72 @@
+log <- function(text, file="log.txt"){
+  tryCatch({
+    log <- file(file, "at")
+    writeLines(text, con=log)
+    close(log)
+  }, error= function(e){}
+  )
+  print(text)
+}
+
+
+log("Loading requried packages")
+library(shiny)
+require(data.table)
+require(grid)
+require(ggplot2)
+
+#load MAP Helper functions
+log("Loading MAP graphing functions")
+
+source("lib/MAP_helper_functions.R")
+
+#need to load one-time per session.
+log("Loading dataset...")
+
+map.data <- read.csv("data/map.scores.by.grade.csv")
+map.data <- data.table(map.data)
+
+
+shinyServer(function(input, output) {
+  
+  log("Server init.")
+  
+  getData <- reactive(function(){
+    log("Subsetting dataset...")
+    
+    map.data[Fall12_Grade     == input$grade & 
+               Subject        == input$subject & 
+               SchoolInitials == input$school
+             ]
+    
+  })
+  
+  dataName <- reactive(function(){
+    log("Setting data name...")
+    
+    if (input$dataset == "Geyeser"){
+      return("Eruption Duration (min)")
+    }
+    if (input$dataset == "NYC Wind"){
+      return("Wind Speed (mph)")
+    }
+  })
+  
+  output$main_plot <- renderPlot(function() {
+    log("Plotting MAP Results...")
+  
+    ptitle <- paste0(input$school, " 2012-13 MAP Scores ",input$grade," ",input$subject, "\nFall and Spring RIT Scores vs Expected Growth and College Ready Growth\nby Fall Quartile")
+   
+    p<-plot_MAP_Results_and_Goals(getData(), ptitle, labxpos=100, minx=95,alp=.6) 
+
+    print(p)
+  })
+  
+  output$main_table <- renderDataTable({
+   tbData <- getData()
+   tbData[,list(Fall12_Grade, StudentLastName, StudentFirstName, Subject, Fall12_RIT, Fall12_Pctl, 
+                ProjectedGrowth, CollegeReadyGrowth, Spring13_RIT, Spring13_Pctl)] 
+   }, options = list(bSortClasses=TRUE))
+   
+
+})
---
+++ map/ui.R
@@ -0,0 +1,32 @@
+library(shiny)
+
+shinyUI(pageWithSidebar(
+
+  # Applicaiton Title
+  headerPanel("MAP Shiny Server Test App"),
+  
+  # Selections side bar
+  sidebarPanel(
+    selectInput(inputId = "school",
+                label="School:",
+                choices=c("KAPS", "KAMS", "KCCP"),
+                selected = "KAMS"),
+    
+    selectInput(inputId = "subject",
+                label = "Subject:",
+                choices = c("Mathematics", "Reading"),
+                selected = "Mathematics"),
+    
+    selectInput(inputId = "grade",
+                label = "Grade Level:",
+                choices = c("K", "1", "2", "5", "6", "7", "8"),
+                selected = "5")
+    ),
+  
+  # Main plotting Panel
+  mainPanel(
+    plotOutput(outputId = "main_plot", height = "600px"),
+    dataTableOutput("main_table")
+    )
+  )
+)
